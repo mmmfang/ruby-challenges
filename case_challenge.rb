ac7fbec50a0f978e3459950f2e9dd2fecae674f0
mmmfang <mae.fang@gmail.com>
2015-03-01 13:01:38 -0500

    first commit to ruby challenges repository

---
+++ case_challenge.rb
@@ -0,0 +1,15 @@
+weather = 'foggy'
+case weather
+when 'sunny'
+puts "Add sunscreen."
+when 'rainy' 
+puts "Bring umbrella."
+when 'foggy'
+puts "Don't wear sunglasses."
+when 'cloudy'
+puts "Careful, might rain."
+when 'snowing'
+puts "Make a snowman."
+else
+puts "Unsure, you should check a weather forecast."
+end
---
+++ my_math_method.rb
@@ -0,0 +1,6 @@
+def my_math_method(num1, num2)
+result = num1 + num2
+puts result
+end
+
+my_math_method(4, 5)
